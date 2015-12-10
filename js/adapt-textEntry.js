3618df93ba7d71f6923083171c5b31a62258c47e
RobertPeek <robert@delta-net.co.uk>
2015-12-10 10:18:25 +0000

    Reduced text functionality added

--- js/adapt-textEntry.js
+++ js/adapt-textEntry.js
@@ -8,43 +8,17 @@ define(function(require) {
         events: {
             'click .textEntry-button':'onBtnClicked'
         },
+
+        preRender: function() {
+            // Listen for text change on audio extension
+            this.listenTo(Adapt, "audio:changeText", this.replaceText);
+        },
         
         postRender: function() {
             this.setReadyStatus();
-            this.listenTo(this.model, 'change:_isComplete', this.removeInviewListener);
-            // Check if instruction or body is set, otherwise force completion
-            var cssSelector = this.$('.component-instruction').length > 0 ? '.component-instruction' 
-                : (this.$('.component-body').length > 0 ? '.component-body' : null);
 
-            if (!cssSelector) {
-                this.setCompletionStatus();
-            } else {
-                this.model.set('cssSelector', cssSelector);
-                this.$(cssSelector).on('inview', _.bind(this.inview, this));
-            }
-        },
-
-
-        inview: function(event, visible, visiblePartX, visiblePartY) {
-            if (visible) {
-                if (visiblePartY === 'top') {
-                    this._isVisibleTop = true;
-                } else if (visiblePartY === 'bottom') {
-                    this._isVisibleBottom = true;
-                } else {
-                    this._isVisibleTop = true;
-                    this._isVisibleBottom = true;
-                }
-
-                if (this._isVisibleTop && this._isVisibleBottom) {                   
-                    this.setCompletionStatus();
-                }
-            }
-        },
-
-        removeInviewListener: function(model, changeAttribute) {
-            if (changeAttribute) {
-                this.$(this.model.get('cssSelector')).off('inview');
+            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+                this.replaceText(Adapt.audio.textSize);
             }
         },
 
@@ -59,7 +33,26 @@ define(function(require) {
             this.$('.textEntry-feedback').removeClass('hidden');
             this.$('.textEntry-feedback-body-inner').a11y_on(true);
             this.$('.textEntry-feedback-body-inner').a11y_focus();
+
+            this.setCompletionStatus();
         },
+
+        // Reduced text
+        replaceText: function(value) {
+            // If enabled
+            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+                // Change component title, body and feedback
+                if(value == 0) {
+                    this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
+                    this.$('.component-body-inner').html(this.model.get('body')).a11y_text();
+                    this.$('.textEntry-feedback-body-inner').html(this.model.get('_feedback').body).a11y_text();
+                } else {
+                    this.$('.component-title-inner').html(this.model.get('displayTitleReduced')).a11y_text();
+                    this.$('.component-body-inner').html(this.model.get('bodyReduced')).a11y_text();
+                    this.$('.textEntry-feedback-body-inner').html(this.model.get('_feedback').bodyReduced).a11y_text();
+                }
+            }
+        }
         
     });
     
