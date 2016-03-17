877dc77664334b60a0d4e63fb048306d205a10ba
Dan Gray <dan@sinensis.co.uk>
2016-03-17 14:33:15 +0000

    Add config checks

--- js/adapt-textEntry-audio.js
+++ js/adapt-textEntry-audio.js
@@ -17,7 +17,7 @@ define(function(require) {
         postRender: function() {
             this.setReadyStatus();
 
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 this.replaceText(Adapt.audio.textSize);
             }
         },
@@ -33,7 +33,7 @@ define(function(require) {
             $(event.currentTarget).html(this.model.get("_buttons")._showFeedback.buttonText);
             $(event.currentTarget).attr('aria-label', this.model.get("_buttons")._showFeedback.ariaLabel);
 
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.audio.textSize == 1) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled && Adapt.audio.textSize == 1) {
                 var popupObject = {
                     title: this.model.get("_feedback").titleReduced,
                     body:  this.model.get("_feedback").bodyReduced
@@ -48,7 +48,7 @@ define(function(require) {
             Adapt.trigger("notify:popup", popupObject);
 
             ///// Audio /////
-            if (this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isEnabled && this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
                 // Trigger audio
                 Adapt.trigger('audio:playAudio', this.model.get("_feedback")._audio.src, this.model.get('_id'), this.model.get('_audio')._channel);
             }
@@ -60,7 +60,7 @@ define(function(require) {
         // Reduced text
         replaceText: function(value) {
             // If enabled
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 // Change component title, body and feedback
                 if(value == 0) {
                     this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
