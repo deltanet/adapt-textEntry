9741b45c897e622578e0fe77ddf6005ba42dbf7c
RobertPeek <robert@delta-net.co.uk>
2016-12-05 11:36:27 +0000

    Fixes #7

--- js/adapt-textEntry-audio.js
+++ js/adapt-textEntry-audio.js
@@ -45,8 +45,10 @@ define(function(require) {
 
             ///// Audio /////
             if (this.model.has('_audio') && this.model.get('_audio')._isEnabled && Adapt.audio.audioClip[this.model.get('_audio')._channel].status==1) {
-                // Trigger audio
-                Adapt.trigger('audio:playAudio', this.model.get("_feedback")._audio.src, this.model.get('_id'), this.model.get('_audio')._channel);
+              // Reset onscreen id
+              Adapt.audio.audioClip[this.model.get('_audio')._channel].onscreenID = "";
+              // Trigger audio
+              Adapt.trigger('audio:playAudio', this.model.get("_feedback")._audio.src, this.model.get('_id'), this.model.get('_audio')._channel);
             }
             ///// End of Audio /////
 
